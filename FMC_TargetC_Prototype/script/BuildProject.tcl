69c15c6f5ff0c451ff3f1e284fedf6559d3e63b9
salvador <salvadorventurag@gmail.com>
2019-04-29 10:31:48 -1000

    include nextAddressCnt.vhd to BuildProject.tcl

--- FMC_TargetC_Prototype/script/BuildProject.tcl
+++ FMC_TargetC_Prototype/script/BuildProject.tcl
@@ -72,12 +72,15 @@ set files [list \
  [file normalize "${origin_dir}/hw/src/Round_Buffer/LookupTable_LE.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/Window_BrainV2.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/BlockDelay.vhd"]\
- [file normalize "${origin_dir}/hw/src/Round_Buffer/BitSelector.vhd"]\
+# [file normalize "${origin_dir}/hw/src/Round_Buffer/BitSelector.vhd"]\#
  [file normalize "${origin_dir}/hw/src/Round_Buffer/HammingDecoderV2.vhd"]\
  [file normalize "${origin_dir}/hw/src/TargetC_Control/TARGETC_Control.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/GrayEncoder.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/GrayDecoder.vhd"]\
+ [file normalize "${origin_dir}/hw/src/Round_Buffer/nextAddressCnt.vhd"]\
  [file normalize "${origin_dir}/hw/src/99_Packages/WindowCPU_pkg.vhd"]\
+
+
 ] 
 add_files -norecurse -fileset $obj $files
 
