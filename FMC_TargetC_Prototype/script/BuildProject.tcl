93e1c0c4e8a15e7774e3bd6bcfd97adf4994d7fa
salvador <salvadorventurag@gmail.com>
2019-04-29 12:41:48 -1000

    updating BuildProject.tcl vhdl modules

--- FMC_TargetC_Prototype/script/BuildProject.tcl
+++ FMC_TargetC_Prototype/script/BuildProject.tcl
@@ -70,14 +70,14 @@ set files [list \
  [file normalize "${origin_dir}/hw/src/Round_Buffer/TRIGGER_CONTROLLER.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/SingleTrigger.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/LookupTable_LE.vhd"]\
- [file normalize "${origin_dir}/hw/src/Round_Buffer/Window_BrainV2.vhd"]\
+# [file normalize "${origin_dir}/hw/src/Round_Buffer/Window_BrainV2.vhd"]\
+# [file normalize "${origin_dir}/hw/src/Round_Buffer/BitSelector.vhd"]\
+# [file normalize "${origin_dir}/hw/src/Round_Buffer/HammingDecoderV2.vhd"]\#
  [file normalize "${origin_dir}/hw/src/Round_Buffer/BlockDelay.vhd"]\
- [file normalize "${origin_dir}/hw/src/Round_Buffer/BitSelector.vhd"]\
- [file normalize "${origin_dir}/hw/src/Round_Buffer/HammingDecoderV2.vhd"]\
  [file normalize "${origin_dir}/hw/src/TargetC_Control/TARGETC_Control.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/GrayEncoder.vhd"]\
  [file normalize "${origin_dir}/hw/src/Round_Buffer/GrayDecoder.vhd"]\
- [file normalize "${origin_dir}/hw/src/Round_Buffer/nextAddressCnt.vhd"]\
+ [file normalize "${origin_dir}/hw/src/Round_Buffer/nextAddressCnt.vhdl"]\
  [file normalize "${origin_dir}/hw/src/99_Packages/WindowCPU_pkg.vhd"]\
 
 
