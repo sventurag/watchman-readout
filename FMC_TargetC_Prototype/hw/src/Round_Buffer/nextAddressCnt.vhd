69061a4552ddcb3da84a7abef3d8aafd469a123a
IDLAB-52 <kyho35@hawaii.edu>
2019-05-06 19:17:55 -1000

    address generation halted at 256, rst with CtrlBus_IxSL.WindowStorage

--- FMC_TargetC_Prototype/hw/src/Round_Buffer/nextAddressCnt.vhd
+++ FMC_TargetC_Prototype/hw/src/Round_Buffer/nextAddressCnt.vhd
@@ -14,6 +14,7 @@ entity nextAddressCnt is
 		CLK : in  std_logic;
 		RST : in  std_logic;  -- next address = 0
 		Ctrl : in std_logic;  -- the rising edge of this signal is used as a condition to generate/output the next address
+		usrRst : in std_logic;
 		nextAddress : out std_logic_vector(NBITS-1 downto 0);
 	    nextAddressValid: out std_logic
 	);
@@ -26,6 +27,10 @@ architecture archi of nextAddressCnt is
 	signal reg1: std_logic ;
 	signal reg2: std_logic ;
     signal edge_det: std_logic ;
+    signal reg3: std_logic ;
+    signal reg4: std_logic ;
+    signal edge_det_usrRst: std_logic ;
+    
 
 begin
 
@@ -41,23 +46,41 @@ end process p_edge_detector;
 edge_det <= reg1 and not (reg2);
 
 
-p_counter:	process (CLK, RST) 
+p_edge_detector_usrRst :  process(clk)
+begin
+        if rising_edge (clk) then
+            reg3 <= usrRst;
+            reg4 <= reg3;
+        end if;
+   
+end process p_edge_detector_usrRst;    
+
+edge_det_usrRst <= not(reg3) and  (reg4);
+
+
+
+p_counter:	process (CLK, RST, usrRst) 
 	begin  
-		if RST = '0' then
+		if RST = '0' or edge_det_usrRst = '1' then
 			cnt <= (others => '0');
 			nextAddressValid <= '1';
 	
 		else
 			if rising_edge(clk) then  
 			    if edge_det = '1' then
-					   cnt <= std_logic_vector(unsigned(cnt) + 1);
-					   nextAddressValid<= '1';
+			           if cnt < "11111111" then
+					       cnt <= std_logic_vector(unsigned(cnt) + 1);
+					       nextAddressValid<= '1';
+					   else    
+					       cnt <= "11111111" ;
+					   end if;
 				end if;
 			end if;
 		end if;     
 	end process p_counter; 
 
 	nextAddress <= cnt;
+        
         
     
 end archi;
