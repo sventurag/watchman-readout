5b7a03fd4478f3ae153e55c5c724c8442f75d3b9
Salvador Ventura <salvadorventurag@gmail.com>
2019-06-06 15:12:44 -1000

    updating bd

--- FMC_TargetC_Prototype/hw/bd/base_zynq/sim/base_zynq.vhd
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/sim/base_zynq.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
---Date        : Mon Apr 29 10:50:20 2019
---Host        : negra running 64-bit Ubuntu 18.04.2 LTS
+--Date        : Thu Jun  6 13:05:20 2019
+--Host        : idlab52-OptiPlex-790 running 64-bit Ubuntu 14.04.5 LTS
 --Command     : generate_target base_zynq.bd
 --Design      : base_zynq
 --Purpose     : IP block netlist
