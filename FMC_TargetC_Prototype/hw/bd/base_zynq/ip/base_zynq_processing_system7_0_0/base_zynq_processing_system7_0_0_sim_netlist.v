3cd7372844aed8e5f0a9de961e0b949de5f9919d
Salvador Ventura <salvadorventurag@gmail.com>
2019-07-23 15:42:37 -1000

    binary2file.py functions merged into targetC.py, pedestal in PS from  targetC.py

--- FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_processing_system7_0_0/base_zynq_processing_system7_0_0_sim_netlist.v
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_processing_system7_0_0/base_zynq_processing_system7_0_0_sim_netlist.v
@@ -1,8 +1,8 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
-// Date        : Tue Jun 18 10:46:24 2019
-// Host        : idlab52-OptiPlex-790 running 64-bit Ubuntu 14.04.5 LTS
+// Date        : Tue Jul 23 11:16:22 2019
+// Host        : idlab52-OptiPlex-790 running 64-bit Ubuntu 14.04.6 LTS
 // Command     : write_verilog -force -mode funcsim
 //               /home/idlab-52/salvador_fork/fix_WR_address/watchman-readout/FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_processing_system7_0_0/base_zynq_processing_system7_0_0_sim_netlist.v
 // Design      : base_zynq_processing_system7_0_0
@@ -214,7 +214,7 @@ module base_zynq_processing_system7_0_0
   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI_HP0 WDATA" *) input [31:0]S_AXI_HP0_WDATA;
   (* X_INTERFACE_INFO = "xilinx.com:interface:aximm:1.0 S_AXI_HP0 WSTRB" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME S_AXI_HP0, NUM_WRITE_OUTSTANDING 8, NUM_READ_OUTSTANDING 8, DATA_WIDTH 32, PROTOCOL AXI3, FREQ_HZ 100000000, ID_WIDTH 6, ADDR_WIDTH 32, AWUSER_WIDTH 0, ARUSER_WIDTH 0, WUSER_WIDTH 0, RUSER_WIDTH 0, BUSER_WIDTH 0, READ_WRITE_MODE READ_WRITE, HAS_BURST 1, HAS_LOCK 1, HAS_PROT 1, HAS_CACHE 1, HAS_QOS 1, HAS_REGION 0, HAS_WSTRB 1, HAS_BRESP 1, HAS_RRESP 1, SUPPORTS_NARROW_BURST 0, MAX_BURST_LENGTH 16, PHASE 0.000, CLK_DOMAIN base_zynq_processing_system7_0_0_FCLK_CLK0, NUM_READ_THREADS 1, NUM_WRITE_THREADS 1, RUSER_BITS_PER_BYTE 0, WUSER_BITS_PER_BYTE 0" *) input [3:0]S_AXI_HP0_WSTRB;
   (* X_INTERFACE_INFO = "xilinx.com:signal:interrupt:1.0 IRQ_F2P INTERRUPT" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME IRQ_F2P, SENSITIVITY LEVEL_HIGH:LEVEL_HIGH, PortWidth 2" *) input [1:0]IRQ_F2P;
-  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 FCLK_CLK0 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME FCLK_CLK0, FREQ_HZ 1e+08, PHASE 0.000, CLK_DOMAIN base_zynq_processing_system7_0_0_FCLK_CLK0" *) output FCLK_CLK0;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 FCLK_CLK0 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME FCLK_CLK0, FREQ_HZ 100000000, PHASE 0.000, CLK_DOMAIN base_zynq_processing_system7_0_0_FCLK_CLK0" *) output FCLK_CLK0;
   (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 FCLK_RESET0_N RST" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME FCLK_RESET0_N, POLARITY ACTIVE_LOW" *) output FCLK_RESET0_N;
   (* X_INTERFACE_INFO = "xilinx.com:display_processing_system7:fixedio:1.0 FIXED_IO MIO" *) inout [53:0]MIO;
   (* X_INTERFACE_INFO = "xilinx.com:interface:ddrx:1.0 DDR CAS_N" *) inout DDR_CAS_n;
---
+++ GUI/test_data.py
@@ -0,0 +1,114 @@
+from functools import partial
+from threading import Thread, Timer
+import time
+import sys
+import socket
+import optparse
+import random
+import binary2text as b2t
+import numpy as np
+#from watchman_nogui import Watchman_main_window
+import matplotlib.pyplot as plt
+import waveform_gen_33600 as wv_gen
+from waveform_gen_33600 import wave_gen
+import os
+import targetc as targetc
+import pandas as pd
+
+#fileName = './data/pedestals'
+#pedestal = pd.read_csv(fileName, sep=" ",header=None)[10]
+
+tc = targetc.targetc()
+
+#regID = 78
+
+#regValue = 2340
+#tc.send_command(8, regID, regValue) # nmbrWindows
+#time.sleep(1)
+
+
+wave_gen().Output1(out=False)
+nmbrWindows = 4
+tc.send_command(9,10,nmbrWindows) # pedestal
+time.sleep(3)
+
+#regValue= 0 #
+#regID = 93
+#tc.send_command(8,regID,regValue) # delay from rising edge WR update
+
+time.sleep(1)
+regID = 151
+regValue = 0
+tc.send_command(8,regID,regValue) # first window
+time.sleep(1)
+
+regID = 152
+
+regValue = nmbrWindows
+tc.send_command(8, regID, regValue) # nmbrWindows
+time.sleep(1)
+ 
+wave_gen().Output1(out=True)
+startWindow=0
+totalWindows=8
+
+Windows512 = np.zeros((totalWindows*32))
+Windows512_delays= list()
+
+#tc.send_command(7,0,0)
+delays = list((range(0,10,1)))
+
+#de`lays = list((range(18,19,1)))
+WindowsSum = np.zeros((totalWindows*32))
+
+
+##for j in range(50,63,1):
+#for i in delays:
+##   regID = 65
+##   regValue = j
+##   tc.send_command(8, regID, regValue) 
+##   time.sleep(1)
+##   for k in range(0,10,1):
+#   wave_gen().trigDelay(i*.000000001)
+#   time.sleep(1)
+#   Windows512 = tc.get_512_windows(startWindow,totalWindows,nmbrWindows,2)       
+#  # WindowsSum += Windows512
+#   Windows512 = [int(i)] + Windows512.tolist()
+#   Windows512_delays.append(Windows512)
+
+time.sleep(1)
+regID = 78
+regValue = 500
+tc.send_command(8,regID,regValue) # first window
+time.sleep(1)
+
+
+i=17
+for j in range(50,63,1):
+   #for i in delays:
+   regID = 65
+   regValue = j
+   tc.send_command(8, regID, regValue) 
+   time.sleep(1)
+   for k in range(0,10,1):
+       wave_gen().trigDelay(i*.000000001)
+       time.sleep(1)
+       Windows512 = tc.get_512_windows(startWindow,totalWindows,nmbrWindows,2)       
+      # WindowsSum += Windows512
+       Windows512 = [int(j)] + Windows512.tolist()
+       Windows512_delays.append(Windows512)
+
+#WindowsMean = WindowsSum/(j+1)
+#WindowsAvg = [int(j+1)] + WindowsMean.tolist()
+#Windows512_delays.append(WindowsAvg)
+
+#np.savetxt(os.path.abspath('./data/bb5_qBIAS1300_beforandafter_window17_SweepFB_10times2.txt'), np.array(Windows512_delays).T, fmt='%5.3f')
+np.savetxt(os.path.abspath('./data/bb5_qBIAS_500.txt'), np.array(Windows512_delays).T, fmt='%5.3f')
+
+Windows512_delays = Windows512_delays * 0 
+
+time.sleep(1)
+
+wave_gen().Output1(out=False)
+
+
