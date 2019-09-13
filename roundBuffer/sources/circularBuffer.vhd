fd0d738eabe183dffe8eee90a7e9c56f02ca8582
salvador <salvadorventurag@gmail.com>
2019-09-12 14:22:47 -1000

    roundBuffer simulations state machine w/ 2 states

--- roundBuffer/sources/circularBuffer.vhd
+++ roundBuffer/sources/circularBuffer.vhd
@@ -29,7 +29,7 @@ entity circular_buffer is
           trigger : in std_logic;
           full_fifo : in std_logic;
           
-          ptr_sub : out std_logic_vector(7 downto 0);
+          ptr_sub : out std_logic_vector(8 downto 0);
           ptr_int : out std_logic
    
    --       WR_RS: out std_logic_vector(6 downto 0);
@@ -40,11 +40,10 @@ end circular_buffer;
  
 architecture rtl of circular_buffer is
  
-  signal ptr_sub_i : std_logic_vector(7 downto 0);
+  signal ptr_sub_i : std_logic_vector(8 downto 0);
   signal ptr_int_i : std_logic;
   
---  type stmachine is (start, wr_add, next_int1, hit ,next_int0, full, read_add, next_sub);
- type stmachine is (start, wr_add, next_int1, hit ,next_int0, read_add, next_sub);
+ type stmachine is (start, wr_add, hit);
 
   signal stm: stmachine;
   
@@ -64,38 +63,26 @@ architecture rtl of circular_buffer is
           stm <= wr_add;
       
       when wr_add =>
-          stm <= next_int1;
-      when next_int1 => 
           ptr_int_i <= '1';
           stm <= hit;
+
       when hit =>
           if trigger = '1' then
               if full_fifo = '1' then 
                  stm <= hit;
               else
-                  stm <= read_add;
+                  ptr_sub_i <= std_logic_vector(unsigned(ptr_sub_i) + 1);
+                  ptr_int_i <= '0';
+                  stm <= wr_add;            
+                  
               end if;
           else
-               stm <= next_int0;
-   --            ptr_int_i <= '0';
+               ptr_int_i <= '0';
+               stm <= wr_add;
+
+             
           end if;
---       when full =>
---           if full_fifo ='0' then
---             stm <= read_add;
---           else
---              stm <= full;
---           end if;
-       when read_add =>
---             read_add <= ptr_sub & ptr_int;  
-             stm<=next_sub;
-       when next_sub =>
-            ptr_sub_i <= std_logic_vector(unsigned(ptr_sub_i) + 1);
-            ptr_int_i <= '0';
-            stm <= wr_add;
-            
-       when next_int0 =>
-            ptr_int_i <= '0';
-            stm <= wr_add;
+
             
             end case;
    end if;
