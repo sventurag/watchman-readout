a2b13fcd3032a1f4e4c6efdd96fe8e1938f53a1f
sventura <salvadorventurag@gmail.com>
2019-12-04 17:52:04 -1000

    [after synt and impl] changes for RAMP and working properly

--- FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_auto_pc_0/base_zynq_auto_pc_0_stub.v
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_auto_pc_0/base_zynq_auto_pc_0_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
-// Date        : Tue Dec  3 10:52:38 2019
+// Date        : Wed Dec  4 16:02:04 2019
 // Host        : watchman running 64-bit Ubuntu 18.04.3 LTS
 // Command     : write_verilog -force -mode synth_stub -rename_top base_zynq_auto_pc_0 -prefix
 //               base_zynq_auto_pc_0_ base_zynq_auto_pc_0_stub.v
