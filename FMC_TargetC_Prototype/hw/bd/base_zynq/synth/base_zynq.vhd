c2a1c870368d855bdcba681b4f7daa9ef2f1c7b9
sventura <salvadorventurag@gmail.com>
2019-12-05 15:38:40 -1000

    [after synth and impl] TPG pass!
    Both modes working properly, dig time=29 us

--- FMC_TargetC_Prototype/hw/bd/base_zynq/synth/base_zynq.vhd
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/synth/base_zynq.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
---Date        : Thu Dec  5 14:37:48 2019
+--Date        : Thu Dec  5 15:11:24 2019
 --Host        : watchman running 64-bit Ubuntu 18.04.3 LTS
 --Command     : generate_target base_zynq.bd
 --Design      : base_zynq
