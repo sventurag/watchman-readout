05bcbbaa1d91d32d873703d575def28caeab35fc
sventura <salvadorventurag@gmail.com>
2019-12-11 13:14:35 -1000

    trigger thread and saving data to a txt file

--- GUI/test_triggermode.py
+++ GUI/test_triggermode.py
@@ -18,34 +18,19 @@ from plot_delays_max import plot_pulse
 tc = targetc.targetc()
 fileToSave = './data/trigger.txt'
 
-#for i in range(10):
-#    tc.send_command(3,0,0) # start TriggerMode
+for i in range(0,10,1):
+    Windows512 =  tc.trigger_mode(1,15,fileToSave )
+    plot_pulse(fileToSave)
 #    time.sleep(3)
-#    print(i)
-#wave_gen().Output1(out=True)
+    time.sleep(1)
+    #plt.close(1)
 
-#tc.send_command(3,0,0) # start TriggerMode
-totalWindows=1
-Windows512 = np.zeros((totalWindows*31))
 
-Windows512_delays= list()
-
-Windows512 =  tc.trigger_mode(1,15)
-print(Windows512)
-Windows512 = [1] + Windows512.tolist()
-Windows512_delays.append(Windows512)
-      
-      #time.sleep(0.5)
-
-np.savetxt(os.path.abspath(fileToSave), np.array(Windows512_delays).T, fmt='%5.3f')
-time.sleep(1)
-
-Windows512_delays = Windows512_delays * 0 
-    
-#wave_gen().Output1(out=False)
 print("end")
 
-plot_pulse(fileToSave)
+
+
+
 
 
 
