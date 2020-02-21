db2a613554d1aea681356141b6c5174db1cd8e0a
sventura <salvadorventurag@gmail.com>
2020-02-20 16:57:28 -1000

    Zero output after checkout WindowStore
    from commit d0b6aac0c45b09029e067f8fba3aa1ebf147c33e

--- FMC_TargetC_Prototype/hw/bd/base_zynq/hdl/base_zynq_wrapper.vhd
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/hdl/base_zynq_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
---Date        : Wed Feb 19 15:33:45 2020
+--Date        : Thu Feb 20 15:06:20 2020
 --Host        : watchman running 64-bit Ubuntu 18.04.3 LTS
 --Command     : generate_target base_zynq_wrapper.bd
 --Design      : base_zynq_wrapper
