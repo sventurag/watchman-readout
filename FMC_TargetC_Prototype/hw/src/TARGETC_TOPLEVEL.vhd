ca29989e02d2a95a06013b84a6faf0eee05a4773
sventura <salvadorventurag@gmail.com>
2020-02-24 12:43:32 -1000

    Setting up trigger for positive pulses

--- FMC_TargetC_Prototype/hw/src/TARGETC_TOPLEVEL.vhd
+++ FMC_TargetC_Prototype/hw/src/TARGETC_TOPLEVEL.vhd
@@ -842,7 +842,8 @@ SyncBitCNT_CLR: SyncBit
 	nTrigC <= not TrigC;
 	nTrigD <= not TrigD;
 
-	Trigger_intl <= nTrigD & nTrigC & nTrigB & nTrigA;
+	--Trigger_intl <= nTrigD & nTrigC & nTrigB & nTrigA;
+	Trigger_intl <= TrigD & TrigC & TrigB & TrigA;  --FOR POSITIVE PULSES
 
 	TestStream <= CtrlBusOut_intl.TestStream;
 
