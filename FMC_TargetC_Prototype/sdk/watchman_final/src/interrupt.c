f3918abb735612b23bfde32987210099eb29332e
sventura <salvadorventurag@gmail.com>
2020-03-09 14:49:32 -1000

    Xil_DCacheInvalidateRange commented

--- FMC_TargetC_Prototype/sdk/watchman_final/src/interrupt.c
+++ FMC_TargetC_Prototype/sdk/watchman_final/src/interrupt.c
@@ -256,7 +256,7 @@ void axidma_rx_callback(XAxiDma* AxiDmaInst){
 			//Initiate a new transfer
 //			XAxiDma_SimpleTransfer_hm((unsigned int)inboundRingManager.writePointer, SIZE_DATA_ARRAY_BYT);
 	           StartDmaTransfer((unsigned int *)inboundRingManager.writePointer , SIZE_DATA_ARRAY_BYT);
-		     Xil_DCacheInvalidateRange((UINTPTR)inboundRingManager.writePointer , SIZE_DATA_ARRAY_BYT);
+		//     Xil_DCacheInvalidateRange((unsigned int)inboundRingManager.writePointer , SIZE_DATA_ARRAY_BYT);
 		     //StartDmaTransfer((unsigned int *)inboundRingManager.writePointer, 1030*2*10);
 		  			ControlRegisterWrite(PSBUSY_MASK,DISABLE);
 
