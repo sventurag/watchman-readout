54daa970e79eb752c2e85cc853686547cd6bb14c
sventura <salvadorventurag@gmail.com>
2020-04-11 16:48:51 -1000

    Not significant improvement updating WR at SSTIN falling edge

--- FMC_TargetC_Prototype/hw/bd/base_zynq/hdl/base_zynq_wrapper.vhd
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/hdl/base_zynq_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
---Date        : Sat Apr 11 11:45:43 2020
+--Date        : Sat Apr 11 14:44:17 2020
 --Host        : watchman running 64-bit Ubuntu 18.04.3 LTS
 --Command     : generate_target base_zynq_wrapper.bd
 --Design      : base_zynq_wrapper
