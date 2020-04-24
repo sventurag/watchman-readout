416d99078a0fc35bd82188d71e414820fd92a877
sventura <salvadorventurag@gmail.com>
2020-04-23 19:23:11 -1000

    Configuration to get pulses
    The current threshold values requires the pulses to be at 0 - 0.070 range in order to not  saturate the ADC. There is also a probable relationship between pulse width and noise, to be confirmed. Pedestal must be reviewed as well.

--- GUI/test_triggermode.py
+++ GUI/test_triggermode.py
@@ -52,7 +52,19 @@ def softTrigger():
 #import subprocess
 #subprocess.call("./tcpdumpWatchmanData", cwd="/home/salvador/bin")
 
-setFreq(20000)
+
+wave_gen().loadHz(50)
+wave_gen().volt(0.070)
+
+wave_gen().pulseWidth(12e-9)
+time.sleep(1)
+
+wave_gen().Query()
+time.sleep(1)
+
+
+
+setFreq(10000)
 setDelay(7)
 triggerMode(100)
 softTrigger()
