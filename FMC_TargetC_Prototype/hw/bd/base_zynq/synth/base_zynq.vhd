e8667f87aeb045eb82c13efd9ad9a456f843900f
sventura <salvadorventurag@gmail.com>
2020-05-18 12:33:25 -1000

    INCRWAIT from zero to 32
    Not visible changen in pulse height distribution

--- FMC_TargetC_Prototype/hw/bd/base_zynq/synth/base_zynq.vhd
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/synth/base_zynq.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
---Date        : Fri May 15 20:35:51 2020
---Host        : watchman running 64-bit Ubuntu 18.04.3 LTS
+--Date        : Mon May 18 09:51:14 2020
+--Host        : watchman running 64-bit Ubuntu 18.04.4 LTS
 --Command     : generate_target base_zynq.bd
 --Design      : base_zynq
 --Purpose     : IP block netlist
