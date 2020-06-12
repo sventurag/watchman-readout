02e6fddb87bc1cfb3fcc6339bebecb48379e9b0a
sventura <salvadorventurag@gmail.com>
2020-06-11 22:57:08 -1000

    Auto stash before merge of "PedestalsTriggerMode" and "origin/PedestalsTriggerMode"

--- FMC_TargetC_Prototype/hw/bd/base_zynq/sim/base_zynq.vhd
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/sim/base_zynq.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
---Date        : Tue Jun  9 18:32:45 2020
+--Date        : Thu Jun 11 16:15:39 2020
 --Host        : watchman running 64-bit Ubuntu 18.04.4 LTS
 --Command     : generate_target base_zynq.bd
 --Design      : base_zynq
