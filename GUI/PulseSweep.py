71295e3291250c512b6b26e0f7ea4271f4bce262
sventura <salvadorventurag@gmail.com>
2020-08-30 09:57:02 -1000

    testing

--- GUI/PulseSweep.py
+++ GUI/PulseSweep.py
@@ -14,56 +14,11 @@ import os
 import targetc as targetc
 from pulseGen import pulseGen
 import pandas as pd
-from plot_delays_max import plot_pulse
-#from plotPulse import plot_pulse
-#tc = targetc.targetc()
+
 pg = pulseGen()
-#time.sleep(10)
-#def softTrigger():
-#    wave_gen().softTrigger()
-#
-#def pulseSweepInit(channel, nmbrPedestals):
-#    wave_gen().Output1(out=False)
-#    regID = 99
-#    tc.send_command(8,regID,channel) # channel number
-#    time.sleep(1)
-#    tc.send_command(9,100,1) # Pedestals
-#
-#def pulseInit(width):
-#            
-#    time.sleep(2)
-#    wave_gen().pulseWidth(width)
-#   # time.sleep(2)
-#   # wave_gen().volt(ampl)
-#    #wave_gen().Query()
-#        
-#def pulseAmpl(ampl):
-#    wave_gen().volt(ampl)
-#    time.sleep(1)
-#
-#
-#def windows(nmbrWindows,firstWindow, totalWindows):
-#    regID = 151
-#    regValue = 4
-#    tc.send_command(8,regID,firstWindow) # first window
-#    time.sleep(1)
-#    regID = 152
-#    tc.send_command(8, regID, nmbrWindows) # nmbrWindows
-#    time.sleep(1)
-#    regID = 98
-#    tc.send_command(8,regID,totalWindows) # total windows
-#    time.sleep(1)
-#
-#def setFreq(frequency):
-#    print("{:.9f}".format(frequency))
-#    wave_gen().Freq(frequency)
-#    time.sleep(1)
-##setFreq(1e3)
-##wave_gen().query()
-#setFreq(1e7)
 nmbrWindows = 1
 firstWindow= 0
-totalWindows = 16
+totalWindows = 512
 nmbrPedestals = 100
 channel = 2
 width = 3e-9
@@ -76,10 +31,10 @@ pg.windows(nmbrWindows,firstWindow,totalWindows)
 pg.pulseSweepInit(channel,nmbrPedestals)
 pg.pulseInit(width)
 time.sleep(10)
-wave_gen().Output1(out=True)
+wave_gen().Output1(out=False)
 time.sleep(1)
 
-amplitudes=np.arange(1e-3, 3.5,100e-3) #For dynamic range, charge, cfd, etc
+#amplitudes=np.arange(1e-3, 3.5,100e-3) #For dynamic range, charge, cfd, etc
 #amplitudes=np.arange(20, 200,50) #For dynamic range, charge, cfd, etc
 #wave_gen().ncyc(3)
 time.sleep(1)
@@ -87,7 +42,7 @@ pg.pulseAmpl(ampl)
 time.sleep(1)
 wave_gen().Query()
 #amplitudes = np.arange(2100,2650,50)
-#amplitudes= np.arange(0.1,0.2 ,1)
+amplitudes= np.arange(0.1,0.2 ,1)
 #amplitudes=list(range(2100,2900,20))
 rango = list((range(10,11,1)))  # number of steps in delay values for the waveformigenerator For dynamic range, charge, cfd, etc
 for item in amplitudes:
@@ -106,7 +61,7 @@ for item in amplitudes:
       # pg.softTrigger()
        pg.getWindows()
       # tc.send_command(7,0,0) # get windows
-       time.sleep(0.3)
+       time.sleep(3)
     
 wave_gen().Output1(out=False)
 print("end")
