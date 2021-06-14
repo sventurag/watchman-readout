ddd47992e053f361f74973fc18cf39c40572cf77
Salvador <salvadorventurag@gmail.com>
2021-06-14 10:48:04 -1000

    Noise out of RoundBuffer still there

--- FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_Start_digitization_ip_0_0/base_zynq_Start_digitization_ip_0_0_sim_netlist.vhdl
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_Start_digitization_ip_0_0/base_zynq_Start_digitization_ip_0_0_sim_netlist.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
--- Date        : Sat Jun 12 20:02:02 2021
+-- Date        : Mon Jun 14 09:33:07 2021
 -- Host        : flacaVivado running 64-bit Ubuntu 20.04.2 LTS
 -- Command     : write_vhdl -force -mode funcsim
 --               /home/salvador/github/watchman-readout/FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_Start_digitization_ip_0_0/base_zynq_Start_digitization_ip_0_0_sim_netlist.vhdl
--- FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_TARGET_C_TopLevel_Sy_1_6/base_zynq_TARGET_C_TopLevel_Sy_1_6_stub.v
+++ FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_TARGET_C_TopLevel_Sy_1_6/base_zynq_TARGET_C_TopLevel_Sy_1_6_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
-// Date        : Sat Jun 12 20:05:34 2021
+// Date        : Mon Jun 14 09:36:27 2021
 // Host        : flacaVivado running 64-bit Ubuntu 20.04.2 LTS
 // Command     : write_verilog -force -mode synth_stub
 //               /home/salvador/github/watchman-readout/FMC_TargetC_Prototype/hw/bd/base_zynq/ip/base_zynq_TARGET_C_TopLevel_Sy_1_6/base_zynq_TARGET_C_TopLevel_Sy_1_6_stub.v
