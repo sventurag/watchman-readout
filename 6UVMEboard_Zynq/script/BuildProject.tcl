1faac6f5f2407236526e2d5464eb32d181454d2a
sventurag <salvadorventurag@gmail.com>
2021-07-07 16:12:44 -1000

    delete run configurations

--- 6UVMEboard_Zynq/script/BuildProject.tcl
+++ 6UVMEboard_Zynq/script/BuildProject.tcl
@@ -31,7 +31,7 @@ cd ${proj_dir}
 # Set project properties
 set obj [current_project]
 set_property -name "corecontainer.enable" -value "1" -objects $obj
-set_property -name "part" -value "xc7z010clg400-1" -objects $obj
+set_property -name "part" -value "xc7z020clg400-1" -objects $obj
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
 set_property -name "generate_ip_upgrade_log" -value "0" -objects $obj
 set_property -name "sim.ip.auto_export_scripts" -value "1" -objects $obj
@@ -187,25 +187,6 @@ set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property -name "file_type" -value "XDC" -objects $file_obj
 
 # Create 'synth_1' run (if not found)
-if {[string equal [get_runs -quiet synth_1] ""]} {
-    create_run -name synth_1 -part xc7z020clg484-1 -flow {Vivado Synthesis 2018} -strategy "Vivado Synthesis Defaults" -report_strategy {No Reports} -constrset constrs_1
-} else {
-  set_property strategy "Vivado Synthesis Defaults" [get_runs synth_1]
-  set_property flow "Vivado Synthesis 2018" [get_runs synth_1]
-}
-set obj [get_runs synth_1]
-set_property -name "needs_refresh" -value "1" -objects $obj
-
-# set the current synth run
-current_run -synthesis [get_runs synth_1]
-
-# Create 'impl_1' run (if not found)
-if {[string equal [get_runs -quiet impl_1] ""]} {
-    create_run -name impl_1 -part xc7z020clg484-1 -flow {Vivado Implementation 2018} -strategy "Vivado Implementation Defaults" -report_strategy {No Reports} -constrset constrs_1 -parent_run synth_1
-} else {
-  set_property strategy "Vivado Implementation Defaults" [get_runs impl_1]
-  set_property flow "Vivado Implementation 2018" [get_runs impl_1]
-}
 
 # set the current impl run
 current_run -implementation [get_runs impl_1]
